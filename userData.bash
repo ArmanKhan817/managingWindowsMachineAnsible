dd591e09a84b68b497dcb624af1bcd5f72b3a011
Arman Khan <alarmankhan33@gmail.com>
2024-09-08 19:16:31 +1000

    Update userData.bash

--- userData.bash
+++ userData.bash
@@ -4,7 +4,6 @@ sudo yum install -y ruby
 sudo yum install -y aws-cli
 sudo yum install -y ansible
 cd/home/ubuntu
-aws s3 cp s3://aws-codedeploy-ap-southeast-2/latest/install --region ap-southeast-2
 chmod +x./install
 ./install auto
 ssh-keygen -t rsa -b 4096	
